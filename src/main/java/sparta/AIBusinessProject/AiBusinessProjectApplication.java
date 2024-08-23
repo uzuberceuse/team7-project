adb6608aa1af3866663707812fb0407a3627155c
juoonge <juoonge7646@naver.com>
2024-08-23 12:05:59 +0900

    first commit

---
+++ src/main/java/sparta/AIBusinessProject/AiBusinessProjectApplication.java
@@ -0,0 +1,13 @@
+package sparta.AIBusinessProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBusinessProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBusinessProjectApplication.class, args);
+	}
+
+}
