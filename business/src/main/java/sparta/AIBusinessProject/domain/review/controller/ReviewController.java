c75e58cefa0790db027234b2810990821574d67e
uzuberceuse <cerulesky@naver.com>
2024-08-27 23:12:48 +0900

    REVIEW 컨트롤러, 서비스, 레포지토리 뼈대 완성

--- business/src/main/java/sparta/AIBusinessProject/domain/review/controller/ReviewController.java
+++ business/src/main/java/sparta/AIBusinessProject/domain/review/controller/ReviewController.java
@@ -1,4 +1,59 @@
 package sparta.AIBusinessProject.domain.review.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sparta.AIBusinessProject.domain.product.dto.ProductRequestDto;
+import sparta.AIBusinessProject.domain.review.dto.ReviewDto;
+import sparta.AIBusinessProject.domain.review.service.ReviewService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/review")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+
+        /* 리뷰 등록
+           등록 결과 확인
+        */
+        @PostMapping
+        public ReviewDto createReview(@RequestBody ReviewDto reviewDto,
+                                      @RequestHeader String user_id) {
+            return ;
+        }
+
+
+        /* 리뷰 수정
+           수정 결과 확인
+        */
+        @PatchMapping("/{review_id}")
+        public ReviewDto updateReview(@PathVariable String review_id,
+                                      @RequestBody ReviewDto reviewDto,
+                                      @RequestHeader String user_id) {
+            return ;
+        }
+
+        /* 리뷰 삭제
+           삭제 결과는 T/F
+        */
+        @DeleteMapping("/{review_id")
+        public Boolean deleteProduct(@PathVariable String review_id,
+                                     @RequestBody ProductRequestDto productRequestDto,
+                                     @RequestHeader String user_id) {
+            return ;
+        }
+
+
+        // 리뷰 목록 조회
+        @GetMapping
+        public ResponseEntity<List<ReviewDto>> getReviews() {
+            return ;
+        }
 }
--- business/src/main/java/sparta/AIBusinessProject/domain/review/service/ReviewService.java
+++ business/src/main/java/sparta/AIBusinessProject/domain/review/service/ReviewService.java
@@ -1,4 +1,49 @@
 package sparta.AIBusinessProject.domain.review.service;
 
+import org.springframework.stereotype.Service;
+import sparta.AIBusinessProject.domain.review.dto.ReviewDto;
+import sparta.AIBusinessProject.domain.review.repository.ReviewRepository;
+
+@Service
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+
+    // 리뷰 등록
+    public ReviewDto createReview(ReviewDto reviewDto, String user_id) {
+        try {
+            return ;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    // 리뷰 수정
+    public ReviewDto updateReview(ReviewDto reviewDto, String user_id) {
+        try {
+            return ;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    // 리뷰 삭제
+    public boolean deleteReview(String review_id, String user_id) {
+        try {
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // 리뷰 조회
+    public ReviewDto getReview() {
+        return ;
+    }
 }
