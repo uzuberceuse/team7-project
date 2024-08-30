674a2f3e2a6d587676bfad9074117703a21a2fb7
whitejh <56130599+whitejh@users.noreply.github.com>
2024-08-30 12:34:32 +0900

    Refactor : Order, Payment의 toResponseDto를 static method로 처리하는 것으로 수정

--- business/src/main/java/sparta/AIBusinessProject/domain/payment/controller/PaymentController.java
+++ business/src/main/java/sparta/AIBusinessProject/domain/payment/controller/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import sparta.AIBusinessProject.domain.payment.dto.PaymentRequestDto;
 import sparta.AIBusinessProject.domain.payment.dto.PaymentResponseDto;
@@ -23,7 +24,7 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     // 1. 결제 생성
-    @Secured({"ROLE_MANAGER", "ROLE_CUSTOMER"})
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_CUSTOMER')")
     @PostMapping("/{order_id}")
     public ResponseEntity<PaymentResponseDto> createPayment(@RequestBody PaymentRequestDto requestDto,
                                                             @PathVariable("order_id") UUID orderId) {
@@ -32,7 +33,7 @@ public class PaymentController {
     }
 
     // 2. 결제 취소
-    @Secured({"ROLE_MANAGER", "ROLE_CUSTOMER"})
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_CUSTOMER')")
     @DeleteMapping("/{payment_id}")
     public ResponseEntity<Void> cancelPayment(@PathVariable UUID payment_id) {
         paymentService.cancelPayment(payment_id);
@@ -40,7 +41,7 @@ public class PaymentController {
     }
 
     // 3. 결제 개별 정보 조회
-    @Secured({"ROLE_MANAGER", "ROLE_CUSTOMER"})
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_CUSTOMER')")
     @GetMapping("/{payment_id}")
     public ResponseEntity<PaymentResponseDto> getPayment(@PathVariable UUID payment_id) {
         PaymentResponseDto responseDto = paymentService.getPayment(payment_id);
@@ -48,7 +49,7 @@ public class PaymentController {
     }
 
     // 4. 결제 목록 조회
-    @Secured({"ROLE_MANAGER", "ROLE_CUSTOMER"})
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_CUSTOMER')")
     @GetMapping
     public ResponseEntity<Page<PaymentResponseDto>> getPaymentList(
             @RequestParam(defaultValue = "0") int page,         // 기본 페이지 번호 0
